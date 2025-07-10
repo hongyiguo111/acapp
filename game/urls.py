0b91d8f14e9969948bd48fa0cd746477253a0953
Hongyi Guo <ghy20060524@163.com>
2025-07-10 15:18:37 +0800

    write my first page

---
+++ game/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from game.views import index, play
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("play/", play, name="play")
+]
+
+
+
--- game/views.py
+++ game/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line3 = '<hr>'
+    line4 = '<a href="/play/">进入游戏界面</a>'
+    line2 = '<img src="https://bkimg.cdn.bcebos.com/pic/c2cec3fdfc039245efdd58288f94a4c27d1e25a8" width=1800>'
+    return HttpResponse(line1 + line4 + line3 + line2)
+
+def play(request):
+    line1 = '<h1 style="text-align: center">游戏界面</h1>'
+    line3 = '<hr>'
+    line4 = '<a href="/">返回主页面</a>'
+    line2 = '<img src="https://bkimg.cdn.bcebos.com/pic/4d086e061d950a7bd7da378c0ad162d9f3d3c98f" width=1800>'
+    return HttpResponse(line1 + line4 + line3 + line2)
