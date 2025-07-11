077e9fff0e2567f074b14353b066d615c6e7976b
Hongyi Guo <ghy20060524@163.com>
2025-07-11 19:37:16 +0800

    fix bug from urls/index.py

--- game/urls/index.py
+++ game/urls/index.py
@@ -2,9 +2,8 @@ from django.urls import path, include
 from game.views.index import index
 
 urlpatterns = [
-    path("", name="index"),
-    path("menu/", include("game.urls.menu.index")),
-    path("playground/", include("game.urls.playground.index"),
-    path("settings/", include("game.urls.settings.index")),
-]
-
+        path("", index, name="index"),
+        path("menu/", include("game.urls.menu.index")),
+        path("playground/", include("game.urls.playground.index")),
+        path("settings/", include("game.urls.settings.index")),
+        ]
