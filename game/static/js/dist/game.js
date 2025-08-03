b7c44ebe31f5268729735a72365c877c526369e8
Hongyi Guo <ghy20060524@163.com>
2025-08-03 13:36:56 +0800

    implement acapp acwing oauth2 login

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -644,8 +644,13 @@ class Settings {
     }
 
     start() {
-        this.add_listening_events();
-        this.getinfo();
+        if(this.platform === "ACAPP"){
+            this.getinfo_acapp();
+        }
+        else {
+            this.getinfo_web();
+            this.add_listening_events();
+        }
     }
 
     add_listening_events() {
@@ -835,12 +840,38 @@ class Settings {
         this.$register.show();
     }
 
+    acapp_login(appid, redirect_uri, scope, state) {
+        let outer = this;
+         this.root.AcWingOS.api.oauth2.authorize(appid, redirect_uri, scope, state, function (resp) {
+            console.log(resp);
+            if(resp.result === "success") {
+               outer.username= resp.username;
+               outer.photo = resp.photo;
+               outer.hide();
+               outer.root.menu.show();
+            }
+         });
+    }
+
     login() { // 打开登陆界面
         this.$login.show();
         this.$register.hide();
     }
 
-    getinfo() {
+    getinfo_acapp() {
+        let outer = this;
+        $.ajax({
+            url: "https://app7549.acapp.acwing.com.cn/settings/acwing/acapp/apply_code/",
+            type: "GET",
+            success: function (resp) {
+                if(resp.result === "success"){
+                    outer.acapp_login(resp.appid, resp.redirect_uri, resp.scope, resp.state);
+                }
+            }
+        })
+    }
+
+    getinfo_web() {
         let outer = this;
 
         $.ajax({
